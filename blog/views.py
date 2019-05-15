9029a7191f9043877cf9da837a950ba1e560c7b6
Randy Galeano <randyagaleano@gmail.com>
2019-05-15 11:25:26 -0400

    Same information being displayed except for with template inheritance
    Understanding how to use template inheritance to my advantage

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Randy Galeano',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'May 15, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'And album by Converge',
+        'content': 'Second Post Content',
+        'date_posted': 'May 13, 2019'
+    }    
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
